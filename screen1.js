d79006c3c7f10b732fa78435e57cec5de78ed3a7
Julian Ceipek <julian.ceipek@gmail.com>
2012-10-23 12:21:27 -0400

    Rectangle pops up on type.

--- screen1.js
+++ screen1.js
@@ -1,42 +1,34 @@
-        function writeMessage(messageLayer, message) {
-        var context = messageLayer.getContext();
-        messageLayer.clear();
-        context.font = '18pt Calibri';
-        context.fillStyle = 'black';
-        context.fillText(message, 10, 25);
-      }
+$(function() {
 
-      function initStage() {
-        var stage = new Kinetic.Stage({
-          container: 'screen1Container',
-          width: 578,
-          height: 200
-        });
+  var screen1 = {
+    stage: new Kinetic.Stage({
+            container: 'screen1Container',
+            width: 640,
+            height: 940}),
+    shapesLayer: new Kinetic.Layer()
+    //messageLayer: new Kinetic.Layer()
+  };
+  screen1.stage.add(screen1.shapesLayer);
 
-        var shapesLayer = new Kinetic.Layer();
-        var messageLayer = new Kinetic.Layer();
+  var createRouteItem = function (layer) {
+    var rect = new Kinetic.Rect({
+      x: 239,
+      y: 75,
+      width: 100,
+      height: 50,
+      fill: 'green',
+      align:"center",
+      stroke: 'black',
+      strokeWidth: 4
+    });
+    layer.add(rect);
+  };
 
-	var rect = new Kinetic.Rect({
-          x: 239,
-          y: 75,
-          width: 100,
-          height: 50,
-	fill: 'green',
-	  align:"center",
-          stroke: 'black',
-          strokeWidth: 4
-        });
+  $('#to-field').keyup(function(){
+          console.log($('#to-field').val());
+          //if($('#to-field').val())
+          createRouteItem(screen1.shapesLayer);
+          screen1.shapesLayer.draw();
+  });
 
-	shapesLayer.add(rect);
-        stage.add(shapesLayer);
-        stage.add(messageLayer);
-
-	$('#to-field').keyup(function(){
-		console.log($('#to-field').val());
-		//if($('#to-field').val())
-	});
-      }
-
-      $(function() {
-        initStage();
 });
