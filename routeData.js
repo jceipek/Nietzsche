688342d86842580c5147c499554ffd5a4e544d24
Julian Ceipek <julian.ceipek@gmail.com>
2012-10-24 14:31:50 -0400

    Moved route data to separate file.

---
+++ routeData.js
@@ -0,0 +1,42 @@
+var PossibleRoutes = [];
+
+ROUTE_TYPES = ['CONTACTS', 'PLACES'];
+
+PossibleRoutes.push(
+  {
+    type: 'CONTACTS',
+    nickname: 'Jenna M. Home',
+    name: 'Jenna Mitnick',
+    address: '1534 This Place',
+    location: 'Somerville, MA',
+    mapsData: {
+      // Put Google Maps Data Here
+    }
+  }
+);
+
+PossibleRoutes.push(
+  {
+    type: 'CONTACTS',
+    nickname: 'Josh R. Home',
+    name: 'Josh Randall',
+    address: '183 Thorne Pike',
+    location: 'Watertown, MA',
+    mapsData: {
+      // Put Google Maps Data Here
+    }
+  }
+);
+
+PossibleRoutes.push(
+  {
+    type: 'PLACES',
+    nickname: 'Josh R. Home',
+    name: 'Josh Randall',
+    address: '183 Thorne Pike',
+    location: 'Watertown, MA',
+    mapsData: {
+      // Put Google Maps Data Here
+    }
+  }
+);
--- screen1.js
+++ screen1.js
@@ -1,45 +1,4 @@
-var PossibleRoutes = [];
-
-ROUTE_TYPES = ['CONTACTS', 'PLACES'];
-
-PossibleRoutes.push(
-  {
-    type: 'CONTACTS',
-    nickname: 'Jenna M. Home',
-    name: 'Jenna Mitnick',
-    address: '1534 This Place',
-    location: 'Somerville, MA',
-    mapsData: {
-      // Put Google Maps Data Here
-    }
-  }
-);
-
-PossibleRoutes.push(
-  {
-    type: 'CONTACTS',
-    nickname: 'Josh R. Home',
-    name: 'Josh Randall',
-    address: '183 Thorne Pike',
-    location: 'Watertown, MA',
-    mapsData: {
-      // Put Google Maps Data Here
-    }
-  }
-);
-
-PossibleRoutes.push(
-  {
-    type: 'PLACES',
-    nickname: 'Josh R. Home',
-    name: 'Josh Randall',
-    address: '183 Thorne Pike',
-    location: 'Watertown, MA',
-    mapsData: {
-      // Put Google Maps Data Here
-    }
-  }
-);
+// NOTE: we have global access to all PossibleRoutes and ROUTE_TYPES
 
 $(function() {
 
