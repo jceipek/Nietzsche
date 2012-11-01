19dff98fdf0d9c739b0cc48fc8740fc0b99d9908
Pati Martin <patimartinvicente@gmail.com>
2012-11-01 02:39:33 -0400

    T icon fixed

--- screen1.js
+++ screen1.js
@@ -400,8 +400,8 @@ $(function() {
         y: y+height/2+4
       };
       var iconSideLength = 30;
-      var icon = createBusIcon(iconMid.x-iconSideLength/2, iconMid.y, iconSideLength, 'blue', '6');
-      //var icon = createTIcon(iconMid.x-iconSideLength/2, iconMid.y, iconSideLength);
+      //var icon = createBusIcon(iconMid.x-iconSideLength/2, iconMid.y, iconSideLength, 'blue', '6');
+      var icon = createTIcon(iconMid.x-iconSideLength/2, iconMid.y, iconSideLength);
     
       routeGroup.add(icon);
     }
@@ -456,32 +456,36 @@ $(function() {
     //TODO: Make it that it looks like a real T icon
        var tIcon = new Kinetic.Group();
        var radius = sidelength/2;
+       var insetSide = sidelength*0.13;
+       var insetTop = sidelength*0.21;
+       var thickness = sidelength*0.0363;
+       
+       tIcon.setPosition(x,y);
        
        var verticalRect = new Kinetic.Rect({
-          x: x+sidelength*0.425,
-          y: y+sidelength*0.40,
+          x: sidelength*0.5 - sidelength*0.173/2 + thickness/2,
+          y: sidelength*0.5 - insetTop + thickness*2,
           height: sidelength*0.5,
-          width: sidelength*0.207,
+          width: sidelength*0.173,
           fill: 'black'
        });
        
        var horizontalRect = new Kinetic.Rect({
-          //x: x+sidelength*0.21,
-          //y: y+sidelength*0.13,
-          x: x+sidelength*0.15,
-          y: y+sidelength*0.25,
-          //height: sidelength*0.173,
-          height: sidelength*0.207,
+          x: insetSide+thickness/2,
+          y: insetTop+thickness*2,
+          //x: insetSide + thickness/2,
+          //y: insetTop + thickness/2,
+          height: sidelength*0.173,
           width: sidelength*0.736,
           fill: 'black'
        });
        
        var tIconCircle = new Kinetic.Circle({
-           x: x+radius,
-           y: y+radius,
+           x: radius,
+           y: radius,
            radius: radius,
            stroke: 'black',
-           strokeWidth: sidelength*0.0363
+           strokeWidth: thickness
        });
        
        
