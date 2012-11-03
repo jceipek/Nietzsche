08e953f9c2e6d89c6b4bb48553b83e24ea5cfe81
Laurence Toal <laurence.toal@gmail.com>
2012-11-03 00:11:36 -0400

    new things are good things

---
+++ color-constants.js
@@ -0,0 +1,15 @@
+
+
+var LIST_SELECTED_GRADIENT = [0, 'rgb(5,138,245)', 1, 'rgb(1,93,230)'];
+
+var BUS_STEP_COLOR = '#D3D15F';
+var DRIVING_STEP_COLOR = 'black';
+var WALKING_STEP_COLOR = '#00CED1';
+var UNKNOWN_STEP_COLOR = 'black';
+
+var WAIT_ITEM_BG_COLOR = '#aaa';
+var WAIT_ITEM_TEXT_COLOR = 'white';
+var WAIT_ITEM_BORDER_COLOR = 'black';
+var DIRECTION_STEP_ITEM_BG_COLOR = 'white';
+var DIRECTION_STEP_ITEM_BORDER_COLOR = 'black';
+var DIRECTION_STEP_ITEM_INSTR_COLOR = 'black';
