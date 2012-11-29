2360981f47d8c3c9f023cd1235fbf06375e482f2
Julian Ceipek <julian.ceipek@gmail.com>
2012-11-29 00:29:22 -0500

    Turned off not using Google.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 define(function () {
 
   var App = {
-      use_simulated_data: true,
+      use_simulated_data: false, //Do we use Google Maps or some cached data?
       design_A: true, // Do we use Design A? if not, use Design B
       simulate_delay: true, // Whether to simulate route change introducing delay
       from_route: null,
